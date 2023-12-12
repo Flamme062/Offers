c2cf52d882dff59b3a98c3123c31df7da7d2f0a4
Flamme062 <153607009+Flamme062@users.noreply.github.com>
2023-12-12 11:42:16 +0100

    index.tsx aktualisieren

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,13 +7,4 @@ export default function Home() {
       <Title>Welcome to Swift-Buy</Title>
       <h1>Welcome to Swift-Buy!</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
-}
+      
